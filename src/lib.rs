b021760a231e0afd5065b1c818ab8c0152b36bec
r4nd0m1z3r <38879170+r4nd0m1z3r@users.noreply.github.com>
2022-12-10 15:40:02 +0300

    Refactored macro to structurally store function data.

--- src/lib.rs
+++ src/lib.rs
@@ -6,55 +6,95 @@ use proc_macro::TokenStream as TokenStreamV1;
 use proc_macro2::{Delimiter, Ident, TokenStream as TokenStreamV2, TokenTree};
 use quote::{format_ident, quote};
 
-fn get_fn_name(mut tokens_iter: impl Iterator<Item = TokenTree>) -> Option<(Ident, bool)> {
-    let mut is_next_token_fn_name = false;
-    let mut is_async = false;
-    let fn_name = tokens_iter.find(|token_tree| {
-        if is_next_token_fn_name {
-            return true;
-        }
-        if let TokenTree::Ident(ident) = token_tree {
-            if ident == "async" {
-                is_async = true;
-            }
-            if ident == "fn" {
-                is_next_token_fn_name = true;
-                return false;
-            } else {
-                return false;
-            }
-        }
-        false
-    })?;
-
-    if let TokenTree::Ident(ident) = fn_name {
-        Some((ident, is_async))
-    } else {
-        None
-    }
+#[derive(Debug)]
+struct FnData {
+    is_async: bool,
+    name: Ident,
+    return_type: Option<Ident>,
 }
+impl FnData {
+    fn get_fn_name(token_stream: TokenStreamV2) -> Result<(Ident, bool), ()> {
+        let mut tokens_iter = token_stream.into_iter();
 
-fn get_fn_return_type(tokens_iter: impl Iterator<Item = TokenTree>) -> Option<Ident> {
-    let mut is_next_token_return_type = false;
-    let return_type = tokens_iter.array_chunks::<2>().find(|[token1, token2]| {
-        if is_next_token_return_type {
-            return true;
+        let mut is_next_token_fn_name = false;
+        let mut is_async = false;
+        let fn_name = tokens_iter
+            .find(|token_tree| {
+                if is_next_token_fn_name {
+                    return true;
+                }
+                if let TokenTree::Ident(ident) = token_tree {
+                    if ident == "async" {
+                        is_async = true;
+                    }
+                    if ident == "fn" {
+                        is_next_token_fn_name = true;
+                        return false;
+                    } else {
+                        return false;
+                    }
+                }
+                false
+            })
+            .ok_or(())?;
+
+        if let TokenTree::Ident(ident) = fn_name {
+            Ok((ident, is_async))
+        } else {
+            Err(())
         }
-        if let TokenTree::Punct(punct1) = token1 && let TokenTree::Punct(punct2) = token2 {
-            if punct1.as_char() == '-' && punct2.as_char() == '>' {
-                is_next_token_return_type = true;
-                return false;
-            } else {
-                return false;
+    }
+
+    fn get_fn_return_type(token_stream: TokenStreamV2) -> Option<Ident> {
+        let tokens_iter = token_stream.into_iter();
+
+        let mut return_type_token_index = None;
+        let mut is_next_token_return_type = false;
+        let return_type = tokens_iter.array_chunks::<2>().find(|[token1, token2]| {
+            dbg!(token1, token2);
+            if is_next_token_return_type {
+                return true;
             }
-        }
-        false
-    })?;
+            if let TokenTree::Punct(punct1) = token1 && let TokenTree::Punct(punct2) = token2 {
+                let p1_char = punct1.as_char();
+                let p2_char = punct2.as_char();
 
-    if let TokenTree::Ident(return_type) = return_type[0].clone() {
-        Some(return_type)
-    } else {
-        None
+                if p1_char == '-' && p2_char == '>' {
+                    is_next_token_return_type = true;
+                    return_type_token_index = Some(0);
+                    return false;
+                } else {
+                    return false;
+                }
+            }
+            else if let TokenTree::Punct(punct) = token1 && let TokenTree::Ident(_) = token2 {
+                let p_char = punct.as_char();
+
+                if p_char == '>' {
+                    return_type_token_index = Some(1);
+                    return true;
+                }
+            }
+            false
+        })?;
+
+        if let TokenTree::Ident(return_type) = return_type[return_type_token_index.unwrap()].clone()
+        {
+            Some(return_type)
+        } else {
+            None
+        }
+    }
+
+    fn from_token_stream(token_stream: TokenStreamV2) -> Option<Self> {
+        let return_type = Self::get_fn_return_type(token_stream.clone());
+        let (fn_name, is_async) = Self::get_fn_name(token_stream).ok()?;
+
+        Some(Self {
+            is_async,
+            name: fn_name,
+            return_type,
+        })
     }
 }
 
@@ -91,13 +131,17 @@ fn generate_struct(
 }
 
 fn generate_thunk(
-    is_async: bool,
-    fn_name: &Ident,
+    fn_data: &FnData,
     struct_name: &Ident,
-    return_type: Option<Ident>,
     mut tokens_iter: impl Iterator<Item = TokenTree>,
 ) -> Option<TokenStreamV2> {
-    let thunk_name = format_ident!("{}_thunk", fn_name);
+    let FnData {
+        is_async,
+        name,
+        return_type,
+    } = fn_data;
+
+    let thunk_name = format_ident!("{}_thunk", name);
 
     let struct_tokens = {
         let struct_group = tokens_iter.find(|token_tree| {
@@ -135,33 +179,33 @@ fn generate_thunk(
         .collect::<TokenStreamV2>();
 
     if let Some(return_type) = return_type {
-        if is_async {
+        if *is_async {
             Some(quote! {
                 async fn #thunk_name (args: #struct_name) -> #return_type {
                     let #struct_name { #unwrap_stream } = args;
-                    #fn_name(#unwrap_stream).await
+                    #name(#unwrap_stream).await
                 }
             })
         } else {
             Some(quote! {
                 fn #thunk_name (args: #struct_name) -> #return_type {
                     let #struct_name { #unwrap_stream } = args;
-                    #fn_name(#unwrap_stream)
+                    #name(#unwrap_stream)
                 }
             })
         }
-    } else if is_async {
+    } else if *is_async {
         Some(quote! {
             async fn #thunk_name (args: #struct_name) {
                 let #struct_name { #unwrap_stream } = args;
-                #fn_name(#unwrap_stream).await;
+                #name(#unwrap_stream).await;
             }
         })
     } else {
         Some(quote! {
             fn #thunk_name (args: #struct_name) {
                 let #struct_name { #unwrap_stream } = args;
-                #fn_name(#unwrap_stream);
+                #name(#unwrap_stream);
             }
         })
     }
@@ -172,18 +216,13 @@ pub fn server_function(_attr: TokenStreamV1, item: TokenStreamV1) -> TokenStream
     let item = Into::<TokenStreamV2>::into(item);
     let mut item_iter = item.clone().into_iter();
 
-    let (fn_name, is_async) = get_fn_name(&mut item_iter).expect("Failed to get function name!");
-    let (args_struct, args_struct_name) = generate_struct(&fn_name, &mut item_iter)
+    let fn_data =
+        FnData::from_token_stream(item.clone()).expect("Failed to extract function data!");
+    dbg!(&fn_data);
+    let (args_struct, args_struct_name) = generate_struct(&fn_data.name, &mut item_iter)
         .expect("Failed to generate function arguments struct!");
-    let return_type = get_fn_return_type(&mut item_iter);
-    let thunk = generate_thunk(
-        is_async,
-        &fn_name,
-        &args_struct_name,
-        return_type,
-        args_struct.clone().into_iter(),
-    )
-    .expect("Failed to generate function thunk!");
+    let thunk = generate_thunk(&fn_data, &args_struct_name, args_struct.clone().into_iter())
+        .expect("Failed to generate function thunk!");
 
     quote! {
         #args_struct
